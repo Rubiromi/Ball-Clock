6b1d87413a184222d49978faa9f7e861fbe7f507
Rubiromi <hiromi.deyoung@gmail.com>
2015-03-31 22:28:49 -0600

    second function is working.

--- ball-clock.rb
+++ ball-clock.rb
@@ -1,34 +1,63 @@
 class BallClock
+
   def initialize(balls)
-    @queue = (0.upto(balls - 1)).to_a
+    @balls = balls
+    @queue = (1.upto(@balls)).to_a
     @test = @queue.clone
     @one = []
     @five = []
     @sixty = []
+    @count = 0
   end
 
   def one_cycle
     ball = get_next_ball
 
-    if one.length < 4
-      one << ball
+    if @one.length < 4
+      @one << ball
     else
-      queue += one.reverse
-      one.clear
-      if five.length < 11
-        five << ball
+      @queue += @one.reverse
+      @one.clear
+
+      if @five.length < 11
+        @five << ball
       else
-        queue += five.reverse
-        five.clear
-        if sixty.length < 11
-          sixty << ball
+        @queue += @five.reverse
+        @five.clear
+
+        if @sixty.length < 11
+          @sixty << ball
         else
-          queue += sixty.reverse
-          sixty.clear
-          queue << ball
+          @queue += @sixty.reverse
+          @sixty.clear
+          @queue << ball
+        end
+      end
+    end
+    puts "#{@one} - #{@five} - #{@sixty}"
   end
 
   def get_next_ball
-    queue.shift
+    @queue.shift
+  end
+
+  def run_simulation(num_of_minutes = 0)
+    if num_of_minutes == 0
+      loop do
+        one_cycle
+        @count += 1
+        break if @queue == @test
+      end
+      puts "#{@balls} balls cycle after #{@count / 2} days."
+    else
+      num_of_minutes.times do
+        one_cycle
+        @count += 1
+      end
+      puts "{Min: #{@one}, FiveMin: #{@five} , Hour: #{@sixty}, Main: #{@queue}}"
+    end
   end
 end
+
+bc = BallClock.new(30)
+bc.run_simulation()
